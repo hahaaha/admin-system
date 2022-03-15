b11193afc03c2ae5226a6302a17e4a07a0d4ad6b
haha <390519764@qq.com>
2022-03-15 23:21:30 +0800

    feature: 侧边栏目/主内容区域支持超长滚动

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import { Layout, Menu, Breadcrumb, BackTop } from 'antd';
-import { Routes, Route, useNavigate, Link, useLocation } from 'react-router-dom';
+import { Layout, Menu } from 'antd';
+import { Routes, Route, useNavigate  } from 'react-router-dom';
 import { sidebars } from './config';
 import Home from './components/home';
 import Editor from './components/editor'
 import IconPage from './pages/icon'
 import BackTopPage from './pages/backTop'
+import './App.scss'
 
 const { SubMenu } = Menu;
 const { Header, Content, Sider } = Layout;
@@ -55,49 +56,41 @@ const App: React.FC<{}> = () => {
     }
 
     return (
-        <Layout>
-            <Header className="header">
-                <div className="logo" />
-                <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
-                    <Menu.Item key="1"><Link to="/editor">editor</Link></Menu.Item>
-                    <Menu.Item key="2"><Link to="/home">nav 2</Link></Menu.Item>
-                    <Menu.Item key="3">nav 3</Menu.Item>
+        <Layout className="site">
+            <Sider
+                width={200}
+                className="site-layout-sider"
+            >
+                <Menu
+                    theme="dark"
+                    mode="inline"
+                    defaultSelectedKeys={['editor']}
+                    defaultOpenKeys={['sub1']}
+                    style={{ height: '100%', borderRight: 0 }}
+                    onSelect={menuClick}
+                >
+                    {setMenu()}
                 </Menu>
-            </Header>
-            <Layout>
-                <Sider width={200} className="site-layout-background">
-                    <Menu
-                        mode="inline"
-                        defaultSelectedKeys={['editor']}
-                        defaultOpenKeys={['sub1']}
-                        style={{ height: '100%', borderRight: 0 }}
-                        onSelect={menuClick}
-                    >
-                        {setMenu()}
-                    </Menu>
-                </Sider>
-                <Layout style={{ padding: '0 24px 24px' }}>
-                    <Breadcrumb style={{ margin: '16px 0' }}>
-                        <Breadcrumb.Item>Home</Breadcrumb.Item>
-                        <Breadcrumb.Item>List</Breadcrumb.Item>
-                        <Breadcrumb.Item>App</Breadcrumb.Item>
-                    </Breadcrumb>
-                    <Content
-                        className="site-layout-background"
-                        style={{
-                            padding: 24,
-                            margin: 0,
-                            minHeight: 500,
-                        }}
-                    >
-                        <Routes>
-                            <Route path="/home" element={<Home />} />
-                            <Route path="/editor" element={<Editor />} />
-                            <Route path="/icon" element={<IconPage />} />
-                            <Route path="/backTop" element={<BackTopPage />} />
-                        </Routes>
-                    </Content>
-                </Layout>
+            </Sider>
+            <Layout className="site-layout">
+                <Header 
+                    className="site-layout-header" 
+                    style={{ padding: 0, backgroundColor: '#fff' }}
+                />
+                <Content
+                    style={{
+                        padding: 24,
+                        margin: 0,
+                        minHeight: 500,
+                    }}
+                >
+                    <Routes>
+                        <Route path="/home" element={<Home />} />
+                        <Route path="/editor" element={<Editor />} />
+                        <Route path="/icon" element={<IconPage />} />
+                        <Route path="/backTop" element={<BackTopPage />} />
+                    </Routes>
+                </Content>
             </Layout>
         </Layout>
     );
